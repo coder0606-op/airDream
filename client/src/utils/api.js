c74b0c53f56d3d0d1d7242457cda5e860b38a4c3
Ayush Kumar Sharma <119219104+coder0606-op@users.noreply.github.com>
2026-07-26 18:27:44 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: '/api', // Using proxy in vite
+  import.meta.env.VITE_API_URL,
 });
 
 api.interceptors.request.use((config) => {
